2645dd49ec7465b20a7be168a122d2a86f7a8378
Murphy <git@murphy-in.space>
2023-12-17 19:06:41 +0100

    fix: link article file template bug

--- src/commands/linkArticles.ts
+++ src/commands/linkArticles.ts
@@ -1,4 +1,4 @@
-import { App, Editor } from "obsidian";
+import { App, Editor, Notice, TFile } from "obsidian";
 import { Template, WikipediaSearchSettings } from "../settings";
 import { Article } from "src/utils/searchModal";
 import { SearchModal } from "src/utils/searchModal";
@@ -8,6 +8,7 @@ import { createNoteInFolder } from "src/utils/createNote";
 
 export class LinkArticleModal extends SearchModal {
 	async onChooseSuggestion(article: Article) {
+		console.log(this.settings.templates);
 		if (this.settings.templates.length > 1) {
 			new LinkArticleTemplateModal(app, this.settings, this.editor!, article).open();
 		} else {
@@ -29,7 +30,17 @@ async function insertLink(
 	article: Article,
 	template: Template
 ) {
-	let insert = await generateInsert(settings, article, template.templateString, editor.getSelection());
+	let templateString = template.templateString;
+	if (template.useTemplateFile) {
+		const templateFile = app.vault.getAbstractFileByPath(template.templateFilePath);
+		if (!templateFile || !(templateFile instanceof TFile)) {
+			new Notice(`Aborting! Template file '${template.templateFilePath}' not found!`);
+			return;
+		}
+		templateString = await app.vault.read(templateFile);
+	}
+
+	let insert = await generateInsert(settings, article, templateString, editor.getSelection());
 
 	if (template.createNote) {
 		const path = await createNoteInFolder(
